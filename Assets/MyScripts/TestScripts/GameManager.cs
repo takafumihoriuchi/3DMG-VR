b88193d8c22f7fdc22deccbcb94d060814e5ab9b
Takafumi Horiuchi <horiuchi@ueda.info.waseda.ac.jp>
2021-03-06 16:24:37 +0900

    before trying to set Unity-chan to first-person avatar

---
+++ Assets/MyScripts/TestScripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (OVRManager.display.displayFrequenciesAvailable.Contains(90f))
+            OVRManager.display.displayFrequency = 90f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
