4eb9ed62e87571e9e0f4ee02d75fd5e27059bc92
Takafumi Horiuchi <horiuchi@ueda.info.waseda.ac.jp>
2021-03-09 00:55:31 +0900

    implementing motion with controller shaking

--- Assets/MyScripts/TestScripts/PlayerMotion.cs
+++ Assets/MyScripts/TestScripts/PlayerMotion.cs
@@ -25,11 +25,6 @@ public class PlayerMotion : MonoBehaviour
         characterControllerComponent = OVRPlayerControllerGameObject.GetComponent<CharacterController>();
         OVRPlayerControllerComponent = OVRPlayerControllerGameObject.GetComponent<OVRPlayerController>();
 
-        touchVelocityL = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
-        touchVelocityR = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
-        touchAccelerationL = OVRInput.GetLocalControllerAcceleration(OVRInput.Controller.LTouch);
-        touchAccelerationR = OVRInput.GetLocalControllerAcceleration(OVRInput.Controller.RTouch);
-
         characterControllerComponent.Move(moveDirection * Time.deltaTime);
     }
 
@@ -37,21 +32,29 @@ public class PlayerMotion : MonoBehaviour
     {
         moveDirection = Vector3.zero;
 
-        Debug.Log("ABS(R-touch Y-velocity): " + Math.Abs(touchVelocityL.y));
+        touchVelocityL = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
+        touchVelocityR = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+        touchAccelerationL = OVRInput.GetLocalControllerAcceleration(OVRInput.Controller.LTouch);
+        touchAccelerationR = OVRInput.GetLocalControllerAcceleration(OVRInput.Controller.RTouch);
+
+        Debug.Log("R-touch Y-velocity: " + touchVelocityR.y);
+        Debug.Log("ABS(R-touch Y-velocity): " + Math.Abs(touchVelocityR.y));
 
         bool isWalkMotion =
             (1.0f < Math.Abs(touchVelocityL.y) && Math.Abs(touchVelocityL.y) < 2.5f)
             || (1.0f < Math.Abs(touchVelocityR.y) && Math.Abs(touchVelocityR.y) < 2.5f);
 
         bool isJumpMotion =
-            2.5f < Math.Abs(touchVelocityL.y)
-            || 2.5f < Math.Abs(touchVelocityR.y);
+            2.5f < Math.Abs(touchVelocityL.y) || 2.5f < Math.Abs(touchVelocityR.y);
 
+        // todo 現状のコードではデフォルトのz軸方向に進んでしまう。forward方向に進んで欲しい。
         if (isWalkMotion)
         {
             Debug.Log("walking");
             moveDirection.z = 1.0f;
         }
+
+        // todo 現状のコードではジャンプできていない。
         if (isJumpMotion)
         {
             Debug.Log("jumping");
