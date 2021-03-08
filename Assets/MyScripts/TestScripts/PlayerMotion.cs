ba79934456b34588975b164da423f561480d5425
Takafumi Horiuchi <horiuchi@ueda.info.waseda.ac.jp>
2021-03-08 23:10:37 +0900

    added debug window

--- Assets/MyScripts/TestScripts/PlayerMotion.cs
+++ Assets/MyScripts/TestScripts/PlayerMotion.cs
@@ -7,22 +7,39 @@ public class PlayerMotion : MonoBehaviour
     //[Tooltip("put help-text here")]
     //[SerializeField] GameObject someGameObject = null;
 
-    [SerializeField] GameObject OVRPlayerController = null;
-    CharacterController Controller = null;
+    [SerializeField] GameObject OVRPlayerControllerGameObject = null;
+    CharacterController characterControllerComponent;
+    OVRPlayerController OVRPlayerControllerComponent;
     Vector3 moveDirection = Vector3.zero;
+    Vector3 playerVelocity = Vector3.zero;
+
 
     void Start()
     {
-        Controller = OVRPlayerController.GetComponent<CharacterController>();
-        moveDirection.z = 1.0f;
+        characterControllerComponent = OVRPlayerControllerGameObject.GetComponent<CharacterController>();
+        OVRPlayerControllerComponent = OVRPlayerControllerGameObject.GetComponent<OVRPlayerController>();
+
+        //moveDirection.y = 0.2f; // todo これをコントローラーの加速度で調整する
     }
 
     void Update()
     {
+        characterControllerComponent.Move(moveDirection * Time.deltaTime);
+        Debug.Log(characterControllerComponent.isGrounded);
         if (Input.GetKey(KeyCode.A))
         {
-            Controller.Move(moveDirection);
+            if (characterControllerComponent.isGrounded) {
+                moveDirection.y = 1.0f;
+                characterControllerComponent.Move(moveDirection * Time.deltaTime);
+                moveDirection.y = 0.0f;
+            }
         }
     }
 
 }
+
+// UnityEngine.CharacterController.Move()
+// https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
+// OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch)
+// OVRInput.GetLocalControllerAcceleration(OVRInput.Controller.LTouch)
